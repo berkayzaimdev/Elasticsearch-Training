e1c53bf7a88f00bec7b6cd727729300f53f1258a
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-13 18:06:58 +0300

    Added ProductRepository and Save method

--- src/Elasticsearch.API/Repository/ProductRepository.cs
+++ src/Elasticsearch.API/Repository/ProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Nest;
+﻿using Elasticsearch.API.Models;
+using Nest;
 
 namespace Elasticsearch.API.Repository;
 
@@ -9,5 +10,20 @@ public class ProductRepository
     public ProductRepository(ElasticClient client)
     {
         _client = client;
+    }
+
+    public async Task<Product?> SaveAsync(Product newProduct)
+    {
+        newProduct.Created = DateTime.Now;
+        var response = await _client.IndexAsync(newProduct, x => x.Index("products"));
+
+        if (response.IsValid)
+        {
+            newProduct.Id = response.Id;
+
+            return newProduct;
+        }
+
+        return null;
     }
 }
