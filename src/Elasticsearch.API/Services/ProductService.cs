46751ad1ce0d06e4484f9d631c853b995d9b6ccb
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-14 09:31:57 +0300

    Added ProductCreateDto

---
+++ src/Elasticsearch.API/Services/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using Elasticsearch.API.Repository;
+
+namespace Elasticsearch.API.Services;
+
+public class ProductService
+{
+    private readonly ProductRepository _productRepository;
+
+    public ProductService(ProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task SaveAsync()
+    {
+
+    }
+}
