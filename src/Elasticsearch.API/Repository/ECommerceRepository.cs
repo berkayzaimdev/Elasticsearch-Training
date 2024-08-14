953defa02586244f6aa4bea59f2a98292d83e65d
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-14 12:19:02 +0300

    Added ECommerceRepository

--- src/Elasticsearch.API/Repository/ECommerceRepository.cs
+++ src/Elasticsearch.API/Repository/ECommerceRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Elastic.Clients.Elasticsearch;
+using Elasticsearch.API.Models.ECommerces;
+using System.Collections.Immutable;
 
 namespace Elasticsearch.API.Repository;
 
@@ -10,5 +12,17 @@ public class ECommerceRepository
     public ECommerceRepository(ElasticsearchClient client)
     {
         _client = client;
+    }
+
+    public async Task<ImmutableList<ECommerce>> TermQuery(string customerFirstName)
+    {
+        var result = await _client.SearchAsync<ECommerce>(s => s.Query(
+            q => q.Term(
+                t => t.Field("customer_first_name.keyword"!).Value(customerFirstName)
+                )
+            )
+        );
+
+        return result.Documents.ToImmutableList();
     }
 }
