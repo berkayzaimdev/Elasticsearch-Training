39e7b7567af11e916d6838c179872fe704fc90f2
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-14 12:15:34 +0300

    Added ECommerce entity

---
+++ src/Elasticsearch.API/Models/ECommerces/ECommerce.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Elasticsearch.API.Models.ECommerces;
+
+public class ECommerce
+{
+    [JsonPropertyName("_id")]
+    public string Id { get; set; } = default!;
+
+    [JsonPropertyName("customer_first_name")]
+    public string CustomerFirstName { get; set; } = default!;
+
+    [JsonPropertyName("customer_last_name")]
+    public string CustomerLastName { get; set; } = default!;
+
+    [JsonPropertyName("customer_full_name")]
+    public string CustomerFullName { get; set; } = default!;
+
+    [JsonPropertyName("category")]
+    public string[] Category { get; set; } = default!;
+
+    [JsonPropertyName("order_id")]
+    public int OrderId { get; set; }
+
+    [JsonPropertyName("order_date")]
+    public DateTime OrderDate { get; set; }
+
+    [JsonPropertyName("customer_first_name")]
+    public Product[] Products { get; set; } = default!;
+}
+
+public class Product
+{
+    [JsonPropertyName("product_id")]
+    public long ProductId { get; set; } = default!;
+
+    [JsonPropertyName("product_name")]
+    public string ProductName { get; set; } = default!;
+}
+
---
+++ src/Elasticsearch.API/Repository/ECommerceRepository.cs
@@ -0,0 +1,14 @@
+﻿using Elastic.Clients.Elasticsearch;
+
+namespace Elasticsearch.API.Repository;
+
+public class ECommerceRepository
+{
+    private readonly ElasticsearchClient _client;
+    private const string indexName = "kibana_sample_data_ecommerce";
+
+    public ECommerceRepository(ElasticsearchClient client)
+    {
+        _client = client;
+    }
+}
