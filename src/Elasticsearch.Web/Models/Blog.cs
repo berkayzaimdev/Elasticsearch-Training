ff9ff9638208d642edfa25532887ca2153f102ed
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-15 11:57:29 +0300

    Added Blog entity

--- src/Elasticsearch.Web/Models/Blog.cs
+++ src/Elasticsearch.Web/Models/Blog.cs
@@ -1,5 +1,24 @@
-﻿namespace Elasticsearch.Web.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Elasticsearch.Web.Models;
 
 public class Blog
 {
+    [JsonPropertyName("_id")]
+    public string Id { get; set; } = default!;
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; } = default!;
+
+    [JsonPropertyName("content")]
+    public string Content { get; set; } = default!;
+
+    [JsonPropertyName("tags")]
+    public string[] Tags { get; set; } = default!;
+
+    [JsonPropertyName("user_id")]
+    public Guid UserId { get; set; } = default!;
+
+    [JsonPropertyName("created")]
+    public DateTime Created { get; set; } = default!;
 }
