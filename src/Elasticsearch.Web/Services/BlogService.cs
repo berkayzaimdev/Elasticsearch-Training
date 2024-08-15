29e6384a9cb299365d5651de576198f68115782d
berkayzaimdev <berkayzaimdev@gmail.com>
2024-08-15 12:16:25 +0300

    Created BlogService and Added SaveAsync method

---
+++ src/Elasticsearch.Web/Services/BlogService.cs
@@ -0,0 +1,30 @@
+﻿using Elasticsearch.Web.Models;
+using Elasticsearch.Web.Repositories;
+using Elasticsearch.Web.ViewModels;
+
+namespace Elasticsearch.Web.Services;
+
+public class BlogService
+{
+    private readonly BlogRepository _blogRepository;
+
+    public BlogService(BlogRepository blogRepository)
+    {
+        _blogRepository = blogRepository;
+    }
+
+    public async Task<bool> SaveAsync(BlogCreateViewModel model)
+    {
+        Blog newBlog = new Blog()
+        {
+            Title = model.Title,
+            Content = model.Content,
+            Tags = model.Tags.ToArray(),
+            UserId = Guid.NewGuid()
+        };
+
+        var response = await _blogRepository.SaveAsync(newBlog);
+
+        return response is not null;
+    }
+}
---
+++ src/Elasticsearch.Web/ViewModels/BlogCreateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Elasticsearch.Web.ViewModels;
+
+public class BlogCreateViewModel
+{
+    public string Title { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public List<string> Tags { get; set; } = [];
+}
